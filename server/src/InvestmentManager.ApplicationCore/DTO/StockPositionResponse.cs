a007a527fdfb8002303211259c798fad336681cd
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-03-29 17:55:11 -0400

    feat(app-core): add stock position response dto

---
+++ server/src/InvestmentManager.ApplicationCore/DTO/StockPositionResponse.cs
@@ -0,0 +1,48 @@
+﻿namespace InvestmentManager.ApplicationCore.DTO
+{
+    /// <summary>
+    /// DTO class that represents the return of a single stock position
+    /// </summary>
+    public class StockPositionResponse
+    {
+        /// <summary>
+        /// Unique symbol of the stock position
+        /// </summary>
+        public string Symbol { get; set; }
+
+        /// <summary>
+        /// The number of shares of this stock position
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// The average price of the stock position
+        /// </summary>
+        public double AveragePrice { get; set; }
+
+        /// <summary>
+        /// The cost of the stock position (Quantity * AveragePrice)
+        /// </summary>
+        public double Cost { get; set; }
+
+        /// <summary>
+        /// Current stock price based on latest quote
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// Current stock market value (Quantity * Price)
+        /// </summary>
+        public double MarketValue { get; set; }
+
+        /// <summary>
+        /// Current percentual gain ((Price/AveragePrice) - 1)
+        /// </summary>
+        public double PercentualGain { get; set; }
+
+        /// <summary>
+        /// Current monetary gain (MarketValue - Cost)
+        /// </summary>
+        public double MonetaryGain { get; set; }
+    }
+}
---
+++ server/src/InvestmentManager.ApplicationCore/DTO/StockPositionsResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace InvestmentManager.ApplicationCore.DTO
+{
+    /// <summary>
+    /// DTO class that represents the return of multiple stock positions
+    /// </summary>
+    public class StockPositionsResponse
+    {
+       /// <summary>
+       /// List of stock position response
+       /// </summary>
+       public List<StockPositionResponse>? StockPositions { get; set; }
+    }
+}
