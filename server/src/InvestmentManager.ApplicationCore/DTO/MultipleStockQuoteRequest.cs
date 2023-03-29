cca63443939bfd480e0317fbb09aa1cc9b26b5c9
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-03-29 17:30:53 -0400

    feat(app-core): add multiple stock quote request dto

---
+++ server/src/InvestmentManager.ApplicationCore/DTO/MultipleStockQuoteRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvestmentManager.ApplicationCore.DTO
+{
+    /// <summary>
+    /// DTO for getting multiple stock quotes based on their symbols
+    /// </summary>
+    public class MultipleStockQuoteRequest
+    {
+        /// <summary>
+        /// List containing the stock symbols
+        /// </summary>
+        [Required(ErrorMessage = "Stock symbols list can't be null or empty")]
+        public List<string> Symbols { get; set; }
+    }
+}
