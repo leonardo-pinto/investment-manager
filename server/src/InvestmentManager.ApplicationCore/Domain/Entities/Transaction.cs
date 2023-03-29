429e70ec9fb0cebb737b495ad7ab1df718c6460f
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-03-29 16:54:02 -0400

    feat(app-core): add Transaction entity

---
+++ server/src/InvestmentManager.ApplicationCore/Domain/Entities/Transaction.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvestmentManager.ApplicationCore.Domain.Entities
+{
+    /// <summary>
+    /// Transaction domain model class
+    /// </summary>
+    public class Transaction
+    {
+        /// <summary>
+        /// Unique Id of the transaction
+        /// </summary>
+        [Key]
+        public Guid TransactionId { get; set; }
+
+        /// <summary>
+        /// Unique Id of the position from the transaction
+        /// </summary>
+        public Guid PositionId { get; set; }
+
+        /// <summary>
+        /// Unique Symbol of the transaction
+        /// </summary>
+
+        public string Symbol { get; set; }
+
+        /// <summary>
+        /// The number of shares of this transaction
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// The price of the transaction
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// The cost of the transaction (Quantity * Price)
+        /// </summary>
+        public double Cost { get; set; }
+
+        /// <summary>
+        /// The date and time of the transaction
+        /// </summary>
+        public DateTime DateAndTimeOfTransaction { get; set; }
+
+        /// <summary>
+        /// Type of the transaction (buy or sell)
+        /// </summary>
+        public string TransactionType { get; set; }
+    }
+}
