b348ae7dcb5d9eb04a35e1eee6478d6ec13fe96e
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-03 12:24:54 -0400

    feat(app-core): add IStockPositionRepository interface

---
+++ server/src/InvestmentManager.ApplicationCore/Interfaces/IStockPositionRepository.cs
@@ -0,0 +1,37 @@
+﻿using InvestmentManager.ApplicationCore.Domain.Entities;
+
+namespace InvestmentManager.ApplicationCore.Interfaces
+{
+    /// <summary>
+    /// Represents data access logic for managing StockPosition entity
+    /// </summary>
+    public interface IStockPositionRepository
+    {
+        /// <summary>
+        /// Add a stock position to the data base
+        /// </summary>
+        /// <param name="stockPosition">Stock position object to add</param>
+        /// <returns>Returns the stock position object after adding to the database</returns>
+        Task<StockPosition> AddStockPosition(StockPosition stockPosition);
+
+        /// <summary>
+        /// Updates a stock position object 
+        /// </summary>
+        /// <param name="stockPosition">Stock position to update</param>
+        /// <returns>Returns the stock position object after updating</returns>
+        Task<StockPosition> UpdateStockPosition(StockPosition stockPosition);
+
+        /// <summary>
+        /// Returns all stock position in the database
+        /// </summary>
+        /// <returns>Returns a list containing all stock positions</returns>
+        Task<List<StockPosition>> GetAllStockPositions();
+
+        /// <summary>
+        /// Get a stock position based on its id
+        /// </summary>
+        /// <param name="positionId">Id of the given stock position</param>
+        /// <returns>A stock position object or null</returns>
+        Task<StockPosition?> GetSingleStockPosition(string positionId);
+    }
+}
