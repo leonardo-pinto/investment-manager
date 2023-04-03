de8eff3dc5fdf6c8a2693493fe45aaef40d41eae
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-03 12:25:34 -0400

    feat(app-core): add ITransactionRepository interface

---
+++ server/src/InvestmentManager.ApplicationCore/Interfaces/ITransactionRepository.cs
@@ -0,0 +1,27 @@
+﻿
+using InvestmentManager.ApplicationCore.Domain.Entities;
+using InvestmentManager.ApplicationCore.DTO;
+
+namespace InvestmentManager.ApplicationCore.Interfaces
+{
+    /// <summary>
+    /// Represents data access logic for managing Transaction entity
+    /// </summary>
+    public interface ITransactionRepository
+    {
+        /// <summary>
+        /// Add a transaction to the data base
+        /// </summary>
+        /// <param name="transaction">Transaction object to add</param>
+        /// <returns>Returns the transaction object after adding to the database</returns>
+        Task<Transaction> AddTransaction(Transaction transaction);
+
+
+        /// <summary>
+        /// Get all the transaction for a given stock position
+        /// </summary>
+        /// <param name="positionId">Position id</param>
+        /// <returns>Returns a list containing all transactions</returns>
+        Task<List<TransactionResponse>?> GetTransactionByStockPositionId(string positionId);
+    }
+}
