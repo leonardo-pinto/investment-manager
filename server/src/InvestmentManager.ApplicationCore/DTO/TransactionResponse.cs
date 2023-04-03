dfc5b14d960f145e503d93df1fb4588c6130369f
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-03 09:49:12 -0400

    feat(app-core): add TransactionResponse DTO

---
+++ server/src/InvestmentManager.ApplicationCore/DTO/TransactionResponse.cs
@@ -0,0 +1,40 @@
+﻿using InvestmentManager.ApplicationCore.Enums;
+
+namespace InvestmentManager.ApplicationCore.DTO
+{
+    /// <summary>
+    /// DTO for returning a transaction response
+    /// </summary>
+    public class TransactionResponse
+    {
+        /// <summary>
+        /// Unique symbol of the stock
+        /// </summary>
+        public string Symbol { get; set; }
+
+        /// <summary>
+        /// The number of shares of the transaction
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// The price of the transaction
+        /// </summary>
+        public double Price { get; set; }
+
+        /// <summary>
+        /// The cost of the transaction
+        /// </summary>
+        public double Cost { get; set; }
+
+        /// <summary>
+        /// The date and time of the transaction
+        /// </summary>
+        public DateTime DateAndTimeOfTransaction { get; set; }
+
+        /// <summary>
+        /// The type of the transaction (buy or sell)
+        /// </summary>
+        public TransactionType TransactionType { get; set; }
+    }
+}
---
+++ server/src/InvestmentManager.ApplicationCore/DTO/UpdateStockPositionRequest.cs
@@ -0,0 +1,27 @@
+﻿using InvestmentManager.ApplicationCore.Enums;
+
+namespace InvestmentManager.ApplicationCore.DTO
+{
+    /// <summary>
+    /// DTO for updating a stock position
+    /// </summary>
+    public class UpdateStockPositionRequest
+    {
+        /// <summary>
+        /// Id of the stock position
+        /// </summary>
+        public string PositionId { get; set; }
+
+        /// <summary>
+        /// Quantity of the stock
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Price of the stock
+        /// </summary>
+        public double Price { get; set; }
+
+        public TransactionType TransactionType { get; set; }
+    }
+}
