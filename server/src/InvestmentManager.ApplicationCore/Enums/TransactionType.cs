368ce293d26f97610766e1dbefb5de13565b49fa
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-03 09:48:04 -0400

    chore(app-core): add TransactionType enum description

--- server/src/InvestmentManager.ApplicationCore/Enums/TransactionType.cs
+++ server/src/InvestmentManager.ApplicationCore/Enums/TransactionType.cs
@@ -1,5 +1,8 @@
 ﻿namespace InvestmentManager.ApplicationCore.Enums
 {
+    /// <summary>
+    /// Enum to classify a transaction as Buy or Sell
+    /// </summary>
     public enum TransactionType
     {
         Buy, Sell
