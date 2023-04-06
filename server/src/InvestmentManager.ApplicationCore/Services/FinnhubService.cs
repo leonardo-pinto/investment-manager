a3d097773ea5d32895a876713007b68c4b73df6f
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-06 16:51:52 -0400

    feat(FinnhubService): add GetStockPriceQuote impl

--- server/src/InvestmentManager.ApplicationCore/Services/FinnhubService.cs
+++ server/src/InvestmentManager.ApplicationCore/Services/FinnhubService.cs
@@ -6,9 +6,33 @@ namespace InvestmentManager.ApplicationCore.Services
 {
     public class FinnhubService : IFinnhubService
     {
-        Task<double> IFinnhubService.GetStockPriceQuote(string stockSymbol)
+        private readonly IFinnhubRepository _finnhubRepository;
+
+        public FinnhubService(IFinnhubRepository finnhubRepository)
         {
-            throw new NotImplementedException();
+            _finnhubRepository = finnhubRepository;
+        }
+
+        public async Task<double> GetStockPriceQuote(string stockSymbol)
+        {
+            Dictionary<string, object> responseDictionary = await _finnhubRepository.GetStockPriceQuote(stockSymbol);
+
+            // if the stock symbol is invalid, the api returns "c" as 0 (int type)
+            if (responseDictionary["c"] is int @int)
+            {
+                if (@int == 0)
+                {
+                    throw new ArgumentException("Invalid stock symbol");
+                }
+            }
+
+            double stockPrice = (double)responseDictionary["c"];
+            return stockPrice;
+            //catch (InvalidOperationException ex)
+            //{
+            //    // do something with repository exceptions
+            //    throw new Exception("Catch from single stock quote");
+            //}
         }
 
         public Task<Dictionary<string, double>> GetMultipleStockPriceQuote(List<string> stockSymbols)
