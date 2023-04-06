bcaef45ee30e08b8d20943ff492a43439d021858
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-06 15:46:51 -0400

    test(AddTransactionRequestTest): add unit tests to AddTransactionRequest DTO

---
+++ server/tests/InvestmentManager.UnitTests/DTO/AddTransactionRequestTest.cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using InvestmentManager.ApplicationCore.Domain.Entities;
+using InvestmentManager.ApplicationCore.DTO;
+using InvestmentManager.ApplicationCore.Enums;
+
+namespace InvestmentManager.UnitTests.DTO
+{
+    public class AddTransactionRequestTest
+    {
+        private readonly IFixture _fixture;
+
+        public AddTransactionRequestTest()
+        {
+            _fixture = new Fixture();
+        }
+
+        #region ToTransaction
+
+        [Fact]
+        public void ToTransaction_ToBeSuccessful()
+        {
+            AddTransactionRequest addTransactionRequest =
+                _fixture.Build<AddTransactionRequest>().Create();
+
+            Transaction transaction = addTransactionRequest.ToTransaction();
+
+            double costExpected = addTransactionRequest.Quantity * addTransactionRequest.Price;
+
+            transaction.PositionId.Should().Be(addTransactionRequest.PositionId);
+            transaction.Symbol.Should().Be(addTransactionRequest.Symbol);
+            transaction.Quantity.Should().Be(addTransactionRequest.Quantity);
+            transaction.Price.Should().Be(addTransactionRequest.Price);
+            transaction.Cost.Should().Be(costExpected);
+            transaction.DateAndTimeOfTransaction.Should().Be(addTransactionRequest.DateAndTimeOfTransaction);
+            transaction.TransactionType.Should().Be(addTransactionRequest.TransactionType.ToString());
+        }
+
+        #endregion
+    }
+}
