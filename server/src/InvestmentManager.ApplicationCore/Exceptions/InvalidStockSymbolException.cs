5d0ea0a02b645bade32417af99971393941461f3
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-07 11:17:53 -0400

    feat(exceptions): add InvalidStockSymbolException

---
+++ server/src/InvestmentManager.ApplicationCore/Exceptions/InvalidStockSymbolException.cs
@@ -0,0 +1,14 @@
+﻿namespace InvestmentManager.ApplicationCore.Exceptions
+{
+    /// <summary>
+    /// Represents an invalid stock symbol
+    /// </summary>
+    public class InvalidStockSymbolException : ArgumentException
+    {
+        public InvalidStockSymbolException() : base() { }
+
+        public InvalidStockSymbolException(string? message) : base(message) { }
+
+        public InvalidStockSymbolException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
