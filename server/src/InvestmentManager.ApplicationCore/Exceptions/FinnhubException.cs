bbd9747d92dada9a02537e1e6cb8658d13fa98e7
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-07 10:42:48 -0400

    feat(exceptions): add FinnhubException

---
+++ server/src/InvestmentManager.ApplicationCore/Exceptions/FinnhubException.cs
@@ -0,0 +1,14 @@
+﻿namespace InvestmentManager.ApplicationCore.Exceptions
+{
+    /// <summary>
+    /// Represents a database connection failure
+    /// </summary>
+    public class FinnhubException : Exception
+    {
+        public FinnhubException() { }
+
+        public FinnhubException(string message) : base(message) { }
+
+        public FinnhubException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
