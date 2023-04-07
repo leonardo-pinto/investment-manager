9fb2e790005a30d4a4fc3f8a39d67a3f9e6a997e
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-07 11:17:16 -0400

    feat(exceptions): add InvalidPositionIdException

---
+++ server/src/InvestmentManager.ApplicationCore/Exceptions/InvalidPositionIdException.cs
@@ -0,0 +1,14 @@
+﻿namespace InvestmentManager.ApplicationCore.Exceptions
+{
+    /// <summary>
+    /// Represents an invalid position id
+    /// </summary>
+    public class InvalidPositionIdException : ArgumentException
+    {
+        public InvalidPositionIdException(): base() { }
+
+        public InvalidPositionIdException(string? message) : base(message) { }
+
+        public InvalidPositionIdException(string? message, Exception? innerException) : base(message, innerException) { } 
+    }
+}
