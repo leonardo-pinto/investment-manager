74f2a51594e9772e07e8c7404d81838d2e1ba142
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-08 16:34:52 -0400

    feat(TransactionController): add TransactionController

---
+++ server/src/InvestmentManager.Web/Controllers/TransactionController.cs
@@ -0,0 +1,26 @@
+﻿using InvestmentManager.ApplicationCore.DTO;
+using InvestmentManager.ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvestmentManager.Web.Controllers
+{
+    [Route("api/transactions")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+        [HttpGet("{positionId}")]
+        async public Task<IActionResult> GetAllTransactionsByPositionId(Guid positionId)
+        {
+            List<TransactionResponse> transactionHistory = await _transactionService.GetTransactionHistory(positionId);
+
+            return Ok(transactionHistory);
+        }
+    }
+}
