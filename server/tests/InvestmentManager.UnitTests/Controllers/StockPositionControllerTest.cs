2467cd7754d06299b7dd40ae4ff600d93e6b9c46
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-10 11:43:35 -0400

    test(StockPositionController): add unit tests to CreateStockPosition method

--- server/tests/InvestmentManager.UnitTests/Controllers/StockPositionControllerTest.cs
+++ server/tests/InvestmentManager.UnitTests/Controllers/StockPositionControllerTest.cs
@@ -93,6 +93,8 @@ namespace InvestmentManager.UnitTests.Controllers
 
             // Assert
             result.Should().BeOfType<CreatedAtActionResult>();
+            result.As<CreatedAtActionResult>().RouteValues["positionId"].Should().Be(stockPositionResponse?.PositionId);
+            result.As<CreatedAtActionResult>().Value.Should().BeEquivalentTo(stockPositionResponse);
             _stockPositionServiceMock
                 .Verify(m => m.CreateStockPosition(addStockPositionRequest), Times.Once);
             _transactionServiceMock
