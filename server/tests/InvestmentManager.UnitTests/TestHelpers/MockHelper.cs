af6e7ee0e51167051f321b52cc0b7b38c29376cf
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-10 17:42:11 -0400

    test(TestHelpers): create MockHelper class methods

---
+++ server/tests/InvestmentManager.UnitTests/TestHelpers/MockHelper.cs
@@ -0,0 +1,27 @@
+﻿using AutoFixture;
+using InvestmentManager.ApplicationCore.Domain.Entities;
+
+namespace InvestmentManager.UnitTests.TestHelpers
+{
+    static class MockHelper
+    {
+        private static readonly IFixture _fixture = new Fixture();
+
+        static public string GenerateValidGuidString()
+        {
+            Guid guidId = Guid.NewGuid();
+            return guidId.ToString();
+        }
+
+        static public Dictionary<string, double> GenerateStockPriceDict(List<StockPosition> input)
+        {
+            List<string> stockSymbols = input.Select(e => e.Symbol).ToList();
+            Dictionary<string, double> dict = new();
+            foreach (string symbol in stockSymbols)
+            {
+                dict.Add(symbol, _fixture.Create<double>());
+            }
+            return dict;
+        }
+    }
+}
