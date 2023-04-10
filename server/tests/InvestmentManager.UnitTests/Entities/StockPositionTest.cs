05c9c7f6b82a907aded0e54aa8d08cc28e159d5e
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-10 17:42:43 -0400

    test(Entities): add StockPosition unit tests

---
+++ server/tests/InvestmentManager.UnitTests/Entities/StockPositionTest.cs
@@ -0,0 +1,29 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using InvestmentManager.ApplicationCore.Domain.Entities;
+
+namespace InvestmentManager.UnitTests.Entities
+{
+    public class StockPositionTest
+    {
+        private readonly IFixture _fixture = new Fixture();
+
+        #region UpdateAveragePrice
+
+        [Fact]
+        public void UpdateAveragePrice_ToBeSuccessful()
+        {
+            StockPosition stockPosition = _fixture
+                .Build<StockPosition>()
+                .With(e => e.Quantity, 10)
+                .With(e => e.AveragePrice, 10)
+                .Create();
+
+            double result = stockPosition.UpdateAveragePrice(10, 30);
+
+            result.Should().Be(20);
+        }
+
+        #endregion
+    }
+}
