5c41c3373cfb7346cfaaacac45d59c0b48646eb6
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-10 19:15:58 -0400

    feat(Infrastructure): create initial impl of DbContext

---
+++ server/src/InvestmentManager.Infrastructure/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using InvestmentManager.ApplicationCore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvestmentManager.Infrastructure.AppDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<StockPosition> StockPositions { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
---
+++ server/src/InvestmentManager.Infrastructure/Migrations/20230410231154_InitialCreate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvestmentManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StockPositions",
+                columns: table => new
+                {
+                    PositionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Symbol = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    AveragePrice = table.Column<double>(type: "float", nullable: false),
+                    CurrentPrice = table.Column<double>(type: "float", nullable: false),
+                    Cost = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockPositions", x => x.PositionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    TransactionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PositionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Symbol = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Cost = table.Column<double>(type: "float", nullable: false),
+                    DateAndTimeOfTransaction = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TransactionType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.TransactionId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StockPositions");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
