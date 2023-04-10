d31b64bd984ae5d302455ee78f81f32cee85484c
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-10 17:45:00 -0400

    feat(IStockPositionService): add UpdateStockPriceListBySymbol to interface

--- server/src/InvestmentManager.ApplicationCore/Interfaces/IStockPositionService.cs
+++ server/src/InvestmentManager.ApplicationCore/Interfaces/IStockPositionService.cs
@@ -1,4 +1,5 @@
-﻿using InvestmentManager.ApplicationCore.DTO;
+﻿using InvestmentManager.ApplicationCore.Domain.Entities;
+using InvestmentManager.ApplicationCore.DTO;
 
 namespace InvestmentManager.ApplicationCore.Interfaces
 {
@@ -19,7 +20,7 @@ namespace InvestmentManager.ApplicationCore.Interfaces
         /// </summary>
         /// <param name="updateStockPositionRequest">UpdateStockPositionRequest type</param>
         /// <returns>Returns the position updated details</returns>
-        Task<StockPositionResponse?> UpdateStockPosition(UpdateStockPositionRequest updateStockPositionRequest);
+        Task<StockPositionResponse?> UpdateStockPosition(UpdateStockPositionRequest updateStockPositionRequest, Guid positionId);
 
         /// <summary>
         /// Get all stock positions of a given user
@@ -33,5 +34,15 @@ namespace InvestmentManager.ApplicationCore.Interfaces
         /// <param name="positionId">Position id of the stock</param>
         /// <returns>Returns a StockPositionResponse type</returns>
         Task<StockPositionResponse?> GetSingleStockPosition(Guid positionId);
+
+        /// <summary>
+        /// Given a list of stock symbols and a dictionary containing a price for each symbol,
+        /// it updates all the prices
+        /// </summary>
+        /// <param name="stockPriceDict">Dictionary of stock symbol (key) and price (value)</param>
+        /// <param name="stockPositions">List of stock symbols</param>
+        /// <returns>List of stock position with updated prices</returns>
+        List<StockPosition> UpdateStockPriceListBySymbol(
+            Dictionary<string, double> stockPriceDict, List<StockPosition> stockPositions);
     }
 }
