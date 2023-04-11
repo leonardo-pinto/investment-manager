f1a2af6f401ff5dbc1c9c5f6a6d1f4aa1a7f5861
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-11 18:19:30 -0400

    feat(FinnhubRepository): add FinnhubRrepository implt

--- server/src/InvestmentManager.Infrastructure/Repositories/FinnhubRepository.cs
+++ server/src/InvestmentManager.Infrastructure/Repositories/FinnhubRepository.cs
@@ -1,17 +1,52 @@
 ﻿using InvestmentManager.ApplicationCore.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+using Microsoft.Extensions.Http;
 
 namespace InvestmentManager.Infrastructure.Repositories
 {
     public class FinnhubRepository : IFinnhubRepository
     {
-        Task<double> IFinnhubRepository.GetStockPriceQuote(string stockSymbol)
-        {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuration;
 
-            // if the response is null, throw exception
-            // if the response contains a key error, throw exception
-            // if the response does not contains the "c" key, throw exception
-            // returns the "c" key-value
-            throw new NotImplementedException();
+        public FinnhubRepository(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+        }
+
+        public async Task<double> GetStockPriceQuote(string stockSymbol)
+        {
+            HttpClient httpClient = _httpClientFactory.CreateClient();
+
+            HttpRequestMessage httpRequestMessage = new ()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://finnhub.io/api/v1/quote?symbol={stockSymbol}")
+            };
+
+            httpRequestMessage.Headers.Add(
+                "X-Finnhub-Token", _configuration.GetSection("FinnhubAccessToken").Value);
+
+            HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+            string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+
+            Dictionary<string, double>? responseDict = JsonSerializer.Deserialize<Dictionary<string, double>>(responseBody);
+
+            if (responseDict == null)
+            {
+                throw new InvalidOperationException("No response from server");
+            }
+
+            if (responseDict.ContainsKey("error"))
+            {
+                throw new InvalidOperationException(Convert.ToString(responseDict["error"]));
+            }
+
+            // returns value related to the current price
+            return responseDict["c"];
         }
     }
 }
