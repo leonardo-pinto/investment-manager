1a3c4f461b8bf633a848bad64eadda4edba606b1
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-11 18:16:50 -0400

    feat(Exceptions): add new custom exceptions

---
+++ server/src/InvestmentManager.ApplicationCore/Exceptions/InvalidTransactionTypeException.cs
@@ -0,0 +1,11 @@
+﻿namespace InvestmentManager.ApplicationCore.Exceptions
+{
+    public class InvalidTransactionTypeException : ArgumentException
+    {
+        public InvalidTransactionTypeException() : base() { }
+
+        public InvalidTransactionTypeException(string? message) : base(message) { }
+
+        public InvalidTransactionTypeException(string? message, Exception? innerException) : base(message, innerException) { }
+    }
+}
