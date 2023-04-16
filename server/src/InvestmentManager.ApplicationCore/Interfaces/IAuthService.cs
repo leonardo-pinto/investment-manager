ffb9a5d5de7fcb2bef2e5963ad2e0a66a7871f39
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-16 19:34:24 -0400

    feat(AuthService): add AuthService and interface

---
+++ server/src/InvestmentManager.ApplicationCore/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using InvestmentManager.ApplicationCore.DTO;
+using Microsoft.AspNetCore.Identity;
+
+namespace InvestmentManager.ApplicationCore.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<IdentityResult> Register(RegisterRequest registerRequest);
+        Task<SignInResult> Login(LoginRequest loginRequest);
+        Task<IdentityUser?> FindUserByUserName(string userName);
+    }
+}
