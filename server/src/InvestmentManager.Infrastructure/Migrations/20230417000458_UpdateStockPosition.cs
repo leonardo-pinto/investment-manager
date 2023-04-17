d37c7a978be79ac0434a170fd73bf0b6e25689db
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-16 20:11:07 -0400

    feat(AppContext): add migrations

---
+++ server/src/InvestmentManager.Infrastructure/Migrations/20230417000458_UpdateStockPosition.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvestmentManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateStockPosition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "StockPositions");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentPrice",
+                table: "StockPositions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Cost",
+                table: "StockPositions",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "CurrentPrice",
+                table: "StockPositions",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
