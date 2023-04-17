c0c3b3a924d71bbb83d20bdc5e109f81ded67e20
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-17 10:20:26 -0400

    feat(UsStockQuoteController): add single and multiple stock quotes endpoints

---
+++ server/src/InvestmentManager.Web/Controllers/UsStockQuoteController.cs
@@ -0,0 +1,41 @@
+﻿using InvestmentManager.ApplicationCore.Interfaces;
+using InvestmentManager.ApplicationCore.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace InvestmentManager.Web.Controllers
+{
+    [Route("api/stock-quote/us")]
+    [Authorize]
+    [ApiController]
+    public class UsStockQuoteController : ControllerBase
+    {
+        private readonly IFinnhubService _finnhubService;
+
+        public UsStockQuoteController(IFinnhubService finnhubService)
+        {
+            _finnhubService = finnhubService;
+        }
+
+        [HttpGet("{symbol}")]
+        public async Task<IActionResult> GetSingleUsStockQuote(string symbol)
+        {
+            double stockQuote = await _finnhubService.GetStockPriceQuote(symbol);
+            return Ok(stockQuote);
+        }
+
+        [HttpGet("quotes-list")]
+        public async Task<IActionResult> GetMultipleUsStockQuotes([FromQuery] string symbols) 
+        {
+            string[] symbolsArr = symbols.Split(",");
+            Dictionary<string, double> stockQuotes = await _finnhubService.GetMultipleStockPriceQuote(symbolsArr);
+
+            return Ok(new StockQuotesResponse()
+            {
+                StockQuotes = stockQuotes,
+                UpdatedAt = DateTimeOffset.Now
+            });
+        }
+    }
+}
