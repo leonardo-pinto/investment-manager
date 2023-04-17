5208aa64de1fddbbdb1cd67ed65cc0b05fe8d9a5
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-17 10:20:47 -0400

    feat(StockQuotesResponse): add StockQuotesResponse DTO

---
+++ server/src/InvestmentManager.ApplicationCore/DTO/StockQuotesResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace InvestmentManager.ApplicationCore.DTO
+{
+    public class StockQuotesResponse
+    {
+        public Dictionary<string, double> StockQuotes { get; set; }
+        public DateTimeOffset UpdatedAt { get; set; }
+    }
+}
