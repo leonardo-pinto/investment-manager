a3fb4448d2be746907f6e4a8deda546c87d26c33
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-17 14:43:07 -0400

    refactor(StockQuotesController): chanes us stock specificity to route naming

--- server/src/InvestmentManager.Web/Controllers/StockQuotesController.cs
+++ server/src/InvestmentManager.Web/Controllers/StockQuotesController.cs
@@ -6,26 +6,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace InvestmentManager.Web.Controllers
 {
-    [Route("api/stock-quote/us")]
+    [Route("api/stock-quote")]
     [Authorize]
     [ApiController]
-    public class UsStockQuoteController : ControllerBase
+    public class StockQuotesController : ControllerBase
     {
         private readonly IFinnhubService _finnhubService;
 
-        public UsStockQuoteController(IFinnhubService finnhubService)
+        public StockQuotesController(IFinnhubService finnhubService)
         {
             _finnhubService = finnhubService;
         }
 
-        [HttpGet("{symbol}")]
+        [HttpGet("us/{symbol}")]
         public async Task<IActionResult> GetSingleUsStockQuote(string symbol)
         {
             double stockQuote = await _finnhubService.GetStockPriceQuote(symbol);
             return Ok(stockQuote);
         }
 
-        [HttpGet("quotes-list")]
+        [HttpGet("us/quotes-list")]
         public async Task<IActionResult> GetMultipleUsStockQuotes([FromQuery] string symbols) 
         {
             string[] symbolsArr = symbols.Split(",");
