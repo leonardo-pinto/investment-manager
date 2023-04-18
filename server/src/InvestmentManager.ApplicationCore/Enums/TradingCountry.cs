272aebd53a4ebbb71e355a33eac31bca55e65a3d
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-18 16:30:41 -0400

    feat(TradingCountry): add TradingCountry enum

---
+++ server/src/InvestmentManager.ApplicationCore/Enums/TradingCountry.cs
@@ -0,0 +1,12 @@
+﻿namespace InvestmentManager.ApplicationCore.Enums
+{
+    /// <summary>
+    /// Enum to classify which country the stock is traded
+    /// e.g. US = 0, BR = 1
+    /// </summary>
+    public enum TradingCountry
+    {
+        US, // 0
+        BR // 1
+    }
+}
--- server/src/InvestmentManager.ApplicationCore/Enums/TransactionType.cs
+++ server/src/InvestmentManager.ApplicationCore/Enums/TransactionType.cs
@@ -2,6 +2,7 @@
 {
     /// <summary>
     /// Enum to classify a transaction as Buy or Sell
+    /// e.g. Buy = 0, Sell = 1
     /// </summary>
     public enum TransactionType
     {
