8f331d8aa5733ad4b51bd2cfa270e79db6c37e0e
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-18 08:57:38 -0400

    feat(Program): add Identity config

--- server/src/InvestmentManager.Web/Program.cs
+++ server/src/InvestmentManager.Web/Program.cs
@@ -2,21 +2,60 @@ using InvestmentManager.ApplicationCore.Interfaces;
 using InvestmentManager.ApplicationCore.Services;
 using InvestmentManager.ApplicationCore.Mapper;
 using InvestmentManager.Infrastructure.Repositories;
-using Microsoft.EntityFrameworkCore;
 using InvestmentManager.Infrastructure.AppDbContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddHttpClient();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext"));
 });
 
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options =>
+    {
+        options.Password.RequireDigit = true;
+        options.Password.RequireLowercase = true;
+        options.Password.RequireUppercase = true;
+        options.Password.RequiredLength = 8;
+        options.SignIn.RequireConfirmedAccount = false;
+    })
+    .AddUserManager<UserManager<IdentityUser>>()
+    .AddSignInManager<SignInManager<IdentityUser>>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters()
+        { 
+            ClockSkew = TimeSpan.Zero,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "issuer-key", // move to appsettings.json
+            ValidAudience = "audience-key", // move to appsettings.jsop
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes("1c6942ef04f51b57c999e80bdaa428a7")) // move to secret file
+        };
+    });
+
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 
-// Verify which method to add services to the container
+builder.Services.AddTransient<IAuthService, AuthService>();
+builder.Services.AddTransient<ITokenService, TokenService>();
+builder.Services.AddTransient<IAuthService, AuthService>();
 builder.Services.AddTransient<IStockPositionService, StockPositionService>(); 
 builder.Services.AddTransient<ITransactionService, TransactionService>();
 builder.Services.AddTransient<IFinnhubService, FinnhubService>();
@@ -30,6 +69,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseHsts();
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
