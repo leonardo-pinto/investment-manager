cf9ad6d9c3d78970f9cd8937b03d1e8e1fe7c969
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-20 15:39:01 -0400

    chore(Program): move services config to separate file

--- server/src/InvestmentManager.Web/Program.cs
+++ server/src/InvestmentManager.Web/Program.cs
@@ -1,72 +1,9 @@
-using InvestmentManager.ApplicationCore.Interfaces;
-using InvestmentManager.ApplicationCore.Services;
-using InvestmentManager.ApplicationCore.Mapper;
-using InvestmentManager.Infrastructure.Repositories;
-using InvestmentManager.Infrastructure.AppDbContext;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using InvestmentManager;
 using InvestmentManager.Web.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Logging.ClearProviders().AddConsole().AddDebug();
-
-builder.Services.AddControllers();
-builder.Services.AddHttpClient();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext"));
-});
-
-builder.Services
-    .AddDefaultIdentity<IdentityUser>(options =>
-    {
-        options.Password.RequireDigit = true;
-        options.Password.RequireLowercase = true;
-        options.Password.RequireUppercase = true;
-        options.Password.RequiredLength = 8;
-        options.SignIn.RequireConfirmedAccount = false;
-    })
-    .AddUserManager<UserManager<IdentityUser>>()
-    .AddSignInManager<SignInManager<IdentityUser>>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters()
-        { 
-            ClockSkew = TimeSpan.Zero,
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = "issuer-key", // move to appsettings.json
-            ValidAudience = "audience-key", // move to appsettings.jsop
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes("1c6942ef04f51b57c999e80bdaa428a7")) // move to secret file
-        };
-    });
-
-builder.Services.AddAutoMapper(typeof(MappingProfile));
-
-builder.Services.AddTransient<IBrApiRepository, BrApiRepository>();
-builder.Services.AddTransient<IBrApiService, BrApiService>();  
-builder.Services.AddTransient<IAuthService, AuthService>();
-builder.Services.AddTransient<ITokenService, TokenService>();
-builder.Services.AddTransient<IAuthService, AuthService>();
-builder.Services.AddTransient<IStockPositionService, StockPositionService>(); 
-builder.Services.AddTransient<ITransactionService, TransactionService>();
-builder.Services.AddTransient<IFinnhubService, FinnhubService>();
-builder.Services.AddTransient<IStockPositionRepository, StockPositionRepository>();
-builder.Services.AddTransient<ITransactionRepository, TransactionRepository>();
-builder.Services.AddTransient<IFinnhubRepository, FinnhubRepository>();
-
+builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ server/src/InvestmentManager.Web/StartupExtension/ConfigureServicesExtension.cs
@@ -0,0 +1,74 @@
+﻿using InvestmentManager.ApplicationCore.Interfaces;
+using InvestmentManager.ApplicationCore.Mapper;
+using InvestmentManager.ApplicationCore.Services;
+using InvestmentManager.Infrastructure.AppDbContext;
+using InvestmentManager.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace InvestmentManager
+{
+    public static class ConfigureServicesExtension
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+            services.AddHttpClient();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("ApplicationDbContext"));
+            });
+
+            services
+                .AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    options.Password.RequireDigit = true;
+                    options.Password.RequireLowercase = true;
+                    options.Password.RequireUppercase = true;
+                    options.Password.RequiredLength = 8;
+                    options.SignIn.RequireConfirmedAccount = false;
+                })
+                .AddUserManager<UserManager<IdentityUser>>()
+                .AddSignInManager<SignInManager<IdentityUser>>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ClockSkew = TimeSpan.Zero,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "issuer-key", // move to appsettings.json
+                        ValidAudience = "audience-key", // move to appsettings.jsop
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes("1c6942ef04f51b57c999e80bdaa428a7")) // move to secret file
+                    };
+                });
+
+            services.AddAutoMapper(typeof(MappingProfile));
+
+            services.AddTransient<IBrApiRepository, BrApiRepository>();
+            services.AddTransient<IBrApiService, BrApiService>();
+            services.AddTransient<IAuthService, AuthService>();
+            services.AddTransient<ITokenService, TokenService>();
+            services.AddTransient<IAuthService, AuthService>();
+            services.AddTransient<IStockPositionService, StockPositionService>();
+            services.AddTransient<ITransactionService, TransactionService>();
+            services.AddTransient<IFinnhubService, FinnhubService>();
+            services.AddTransient<IStockPositionRepository, StockPositionRepository>();
+            services.AddTransient<ITransactionRepository, TransactionRepository>();
+            services.AddTransient<IFinnhubRepository, FinnhubRepository>();
+
+            return services;
+        }
+    }
+}
