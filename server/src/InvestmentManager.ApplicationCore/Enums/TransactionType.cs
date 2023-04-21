be6ada195edcc247c9ba376bed2a98c6078b8164
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-04-20 20:53:59 -0400

    refactor(Enums): add custom validation of enum type

--- server/src/InvestmentManager.ApplicationCore/Enums/TransactionType.cs
+++ server/src/InvestmentManager.ApplicationCore/Enums/TransactionType.cs
@@ -2,11 +2,10 @@
 {
     /// <summary>
     /// Enum to classify a transaction as Buy or Sell
-    /// e.g. Buy = 0, Sell = 1
     /// </summary>
     public enum TransactionType
     {
-        Buy, // 0
-        Sell // 1
+        Buy,
+        Sell
     }
 }
