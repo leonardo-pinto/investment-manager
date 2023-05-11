c4a73a9b4f768dee7ded72607691d2a521b6a213
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-05-11 17:12:57 -0400

    feat(common): add BaseButton and BaseCard common components

---
+++ client/src/common/components/BaseCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  border-radius: 10px;
+  box-shadow: 0 2px 6px #454545;
+  margin: 1rem auto;
+  max-width: 80%;
+  padding: 1rem;
+}
+</style>
