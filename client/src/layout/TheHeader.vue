e4221b8ec7451ba3897f3cefb00549a08a014d06
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-05-15 17:52:52 -0400

    feat(layout): add TheHeader component

---
+++ client/src/layout/TheHeader.vue
@@ -0,0 +1,68 @@
+<template>
+  <header>
+    <nav>
+      <h1><router-link to="/">Investment Manager</router-link></h1>
+      <ul v-if="!isAuth">
+        <li>
+          <BaseButton link to="/register">Register</BaseButton>
+        </li>
+        <li>
+          <BaseButton mode="outline" link to="/login">Login</BaseButton>
+        </li>
+      </ul>
+      <ul v-else>
+        <li>
+          <BaseButton link to="/stock-positions">Stock Positions</BaseButton>
+        </li>
+        <li>
+          <BaseButton link to="/transactions">Transactions</BaseButton>
+        </li>
+        <li>
+          <BaseButton link to="/transactions" mode="outline">Logout</BaseButton>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useStore } from '../store';
+
+const store = useStore();
+const isAuth = computed<boolean>(() => store.getters['auth/isAuthenticated']);
+</script>
+
+<style scoped>
+header {
+  align-items: center;
+  background-color: #fff;
+  display: flex;
+  height: 8rem;
+  justify-content: center;
+  width: 100%;
+}
+
+header nav {
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  width: 90%;
+}
+
+header ul {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  list-style: none;
+}
+
+header a {
+  padding: 0.75rem 1.25rem;
+  text-decoration: none;
+}
+
+li {
+  margin: 0 0.5rem;
+}
+</style>
