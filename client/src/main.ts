d9a2560ab8307bba1b5be9179d4ac0991a4431ef
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-05-18 19:40:30 -0400

    feat(main): add main.ts file

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { store, key } from './store';
 
-createApp(App).mount('#app')
+import BaseButton from './common/components/BaseButton.vue';
+import BaseCard from './common/components/BaseCard.vue';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(store, key);
+
+app.component('BaseButton', BaseButton);
+app.component('BaseCard', BaseCard);
+
+app.mount('#app');
