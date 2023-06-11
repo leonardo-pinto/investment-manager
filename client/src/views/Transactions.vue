a09a105660adda6b82ad7ef35342f31e1075094a
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-06-11 11:32:53 -0400

    feat(Transactions): add TransactionsFilter

--- client/src/views/Transactions.vue
+++ client/src/views/Transactions.vue
@@ -1,54 +1,79 @@
 <template>
   Transactions view
-  <select
-    name="tradingCountry"
-    id="tradingCountry"
-    v-model="selectedTradingCountry"
-  >
-    <option :value="TradingCountry.US">US</option>
-    <option :value="TradingCountry.BR">BR</option>
-  </select>
-
-  <select>
-    Type:
-  </select>
-
-  <select>
-    Symbols
-  </select>
-
-  <p>View Range:</p>
-  <p>View dates:</p>
-
   <h1 v-if="isLoading">LOADING...</h1>
-  <h2 v-else-if="!filteredTransactions.length">
-    There are no transactions for {{ selectedTradingCountry }}
-  </h2>
+  <h2 v-else-if="!filteredTransactions.length">There are no transactions</h2>
+  <TransactionsFilter @changeFilters="setFilters" />
   <TransactionsTable
     :filteredTransactions="filteredTransactions"
   ></TransactionsTable>
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
-import { TradingCountry } from '../enums';
+import { computed, ref, reactive } from 'vue';
 import { useStore } from '../store';
 import TransactionsTable from '../components/transactions/TransactionsTable.vue';
+import TransactionsFilter from '../components/transactions/TransactionsFilter.vue';
+import { Transaction } from '../types/transactions';
 
 const store = useStore();
 const isLoading = ref(false);
 
-const selectedTradingCountry = ref<TradingCountry>(
-  store.getters['stockPositions/getSelectedCountry']
-);
-
-watch(selectedTradingCountry, (value) => {
-  store.dispatch('stockPositions/setSelectedTradingCountry', value);
-  getTransactions();
+const selectedFilters = reactive({
+  tradingCountry: store.getters['stockPositions/getSelectedCountry'],
+  transactionType: '',
+  symbol: '',
+  startDate: '',
+  endDate: '',
 });
 
+function setFilters(filters: any) {
+  selectedFilters.tradingCountry = filters.tradingCountry;
+  selectedFilters.transactionType = filters.transactionType;
+  selectedFilters.symbol = filters.symbol;
+  selectedFilters.startDate = filters.startDate;
+  selectedFilters.endDate = filters.endDate;
+}
+
 const filteredTransactions = computed(() => {
-  return store.getters['transactions/getTransactions'];
+  const transactions: Transaction[] =
+    store.getters['transactions/getTransactions'];
+  return transactions.filter(
+    ({ tradingCountry, transactionType, symbol, dateAndTimeOfTransaction }) => {
+      const formattedDate = dateAndTimeOfTransaction.split('T')[0];
+
+      if (
+        selectedFilters.tradingCountry &&
+        selectedFilters.tradingCountry !== tradingCountry
+      ) {
+        return false;
+      }
+
+      if (
+        selectedFilters.transactionType &&
+        selectedFilters.transactionType !== transactionType
+      ) {
+        return false;
+      }
+
+      if (
+        selectedFilters.symbol &&
+        selectedFilters.symbol.toUpperCase() !== symbol
+      ) {
+        return false;
+      }
+
+      if (
+        selectedFilters.startDate &&
+        selectedFilters.endDate &&
+        (selectedFilters.startDate > formattedDate ||
+          selectedFilters.endDate < formattedDate)
+      ) {
+        return false;
+      }
+
+      return true;
+    }
+  );
 });
 
 const getTransactions = () => {
