2d80c984c79b39a40066707effcfcbc26fb26966
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-06-11 11:33:27 -0400

    refactor(Transaction): updates Transaction types

--- client/src/types/transactions.d.ts
+++ client/src/types/transactions.d.ts
@@ -1,8 +1,14 @@
+import { TransactionType } from '../enums';
+
 export interface Transaction {
   symbol: string;
   quantity: number;
   price: number;
-  cost: number;
   dateAndTimeOfTransaction: string;
   transactionType: string;
+  tradingCountry: TransactionType;
+}
+
+export interface Transactions {
+  transactions: Transaction[];
 }
