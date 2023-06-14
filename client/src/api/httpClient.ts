8c5749e6b70c4294cd3dd9e949bc4c1d58107c3a
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-06-14 10:55:12 -0400

    feat(httpClient): add error andn response interceptors

--- client/src/api/httpClient.ts
+++ client/src/api/httpClient.ts
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import router from '../router';
 
 const httpClient = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
-  // add timeout?
 });
 
 const getAuthToken = () => localStorage.getItem('token');
@@ -15,6 +15,26 @@ const authInterceptor = (config: any) => {
   return config;
 };
 
+const responseInterceptor = (response: AxiosResponse) => response;
+
+const unauthErrorInterceptor = (error: any) => {
+  if (
+    error?.response?.status === 401 &&
+    router.currentRoute.value.meta.requiresAuth
+  ) {
+    localStorage.removeItem('userId');
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    router.push('/register');
+  }
+
+  return Promise.reject(error);
+};
+
 httpClient.interceptors.request.use(authInterceptor);
+httpClient.interceptors.response.use(
+  responseInterceptor,
+  unauthErrorInterceptor
+);
 
 export default httpClient;
