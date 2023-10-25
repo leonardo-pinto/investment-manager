273b410250afd0f696f47f93b5d625fb8cbe5dc8
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-10-25 10:28:56 -0400

    refactor(auth-login): uses Vuetify validation instead of VeeValidate

--- client/src/views/AuthLogin.vue
+++ client/src/views/AuthLogin.vue
@@ -6,28 +6,28 @@
     max-width="448"
     rounded="lg"
   >
-    <form @submit.prevent="submitForm">
+    <v-form ref="form" validate-on="blur lazy" @submit.prevent="submitForm">
       <v-container>
         <v-col>
           <v-row>
             <v-text-field
               label="Username"
-              v-model.trim="userName.value.value"
+              v-model.trim="userName"
               prepend-inner-icon="mdi-account"
               density="compact"
-              :error-messages="userName.errorMessage.value"
+              :rules="userNameRules"
             ></v-text-field>
           </v-row>
           <v-row>
             <v-text-field
               label="Password"
-              v-model.trim="password.value.value"
+              v-model.trim="password"
               :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
               :type="visible ? 'text' : 'password'"
               density="compact"
               prepend-inner-icon="mdi-lock-outline"
               @click:append-inner="visible = !visible"
-              :error-messages="password.errorMessage.value"
+              :rules="passwordRules"
             ></v-text-field>
           </v-row>
           <v-row class="d-flex justify-center">
@@ -41,39 +41,32 @@
           </v-row>
         </v-col>
       </v-container>
-    </form>
+    </v-form>
   </v-card>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { useField, useForm } from 'vee-validate';
+import { AuthLoginRequest } from '../types/auth';
 import { useStore } from '../store';
 import { useRouter } from 'vue-router';
-import { AuthLoginRequest } from '../types/auth';
 import { useLoading } from 'vue-loading-overlay';
-import * as yup from 'yup';
+import {
+  requiredField,
+  passwordPattern,
+} from '../common/helpers/validationRules';
 
 const store = useStore();
 const router = useRouter();
+const userName = ref<string>('');
+const password = ref<string>('');
+const form = ref();
 
-const validationSchema = yup.object().shape({
-  userName: yup.string().required('Username is required'),
-  password: yup
-    .string()
-    .matches(
-      /^(?=.*[A-Z])(?=.*\W).{8,}$/,
-      'Password must contain at least 8 characters including one non alphanumeric and one upper case character.'
-    )
-    .required('Password is required'),
-});
-
-const { handleSubmit } = useForm({
-  validationSchema,
-});
-
-const userName = useField<string>('userName', validationSchema);
-const password = useField<string>('password', validationSchema);
+const userNameRules = [(value: string) => requiredField(value, 'Username')];
+const passwordRules = [
+  (value: string) => requiredField(value, 'Password'),
+  (value: string) => passwordPattern(value),
+];
 
 const $loading = useLoading({
   color: '#ff6000',
@@ -83,21 +76,24 @@ const visible = ref(false);
 
 const apiResponseError = ref('');
 
-const submitForm = handleSubmit(async () => {
-  const authCredentials: AuthLoginRequest = {
-    userName: userName.value.value,
-    password: password.value.value,
-  };
-  const loader = $loading.show();
-  try {
-    await store.dispatch('auth/login', authCredentials);
-    router.replace('/stock-positions');
-  } catch (error) {
-    apiResponseError.value = (error as any).response?.data?.error;
-  } finally {
-    loader.hide();
+async function submitForm() {
+  const { valid } = await form.value?.validate();
+  if (valid) {
+    const authCredentials: AuthLoginRequest = {
+      userName: userName.value,
+      password: password.value,
+    };
+    const loader = $loading.show();
+    try {
+      await store.dispatch('auth/login', authCredentials);
+      router.replace('/stock-positions');
+    } catch (error) {
+      apiResponseError.value = (error as any).response?.data?.error;
+    } finally {
+      loader.hide();
+    }
   }
-});
+}
 </script>
 
 <style scoped>
