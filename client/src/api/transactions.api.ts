1db0be2ef17097b3ccf6fd573e735971b4aaf95f
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-10-26 16:25:46 -0400

    chore(transactions-api): remove unused userid

--- client/src/api/transactions.api.ts
+++ client/src/api/transactions.api.ts
@@ -3,12 +3,9 @@ import httpClient from './httpClient';
 
 const TRANSACTIONS_ROUTE = '/transactions';
 
-const getAllTransactions = async (userId: string): Promise<Transaction[]> => {
-  return (
-    await httpClient.get<Transactions>(
-      `${TRANSACTIONS_ROUTE}/user-id/${userId}`
-    )
-  ).data.transactions;
+const getAllTransactions = async (): Promise<Transaction[]> => {
+  return (await httpClient.get<Transactions>(`${TRANSACTIONS_ROUTE}`)).data
+    .transactions;
 };
 
 export { getAllTransactions };
