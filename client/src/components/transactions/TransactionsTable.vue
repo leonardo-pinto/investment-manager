7208bc4f368bb7dd9736aa713eaa37f5e06f266b
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-10-26 16:23:44 -0400

    refactor(transactions-table): add vuetify componentn

--- client/src/components/transactions/TransactionsTable.vue
+++ client/src/components/transactions/TransactionsTable.vue
@@ -1,32 +1,35 @@
 <template>
-  <table id="transactions-table">
-    <thead>
-      <tr>
-        <th>Date/Time</th>
-        <th>Description</th>
-        <th>Amount</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(transaction, index) in props.transactions" :key="index">
-        <td id="date-column">
-          {{ formatDate(transaction.dateAndTimeOfTransaction) }}
-        </td>
-        <td>{{ buildDescription(transaction) }}</td>
-        <td :style="{ color: isBuy(transaction) }">
-          {{ props.currency }}
-          {{ calculateAmount(transaction) }}
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <v-expansion-panels>
+    <v-expansion-panel>
+      <v-expansion-panel-title>Transactions</v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <v-data-table
+          v-model:items-per-page="itemsPerPage"
+          :headers="headers"
+          :items="processedTransactions"
+        >
+          <template v-slot:item.amount="{ value }">
+            <td :style="{ color: getColor(value) }">{{ value }}</td>
+          </template>
+        </v-data-table>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script setup lang="ts">
-// import { ref } from 'vue';
+import { VDataTable } from 'vuetify/lib/labs/VDataTable/index.mjs';
 import { Transaction } from '../../types/transactions';
-import { TransactionType } from '../../enums';
 import { formatDate } from '../../common/helpers';
+import { TransactionType } from '../../enums';
+
+// https://stackoverflow.com/questions/75991355/import-datatableheader-typescript-type-of-vuetify3-v-data-table
+// followed this solution to fix the headers type
+type UnwrapReadonlyArrayType<A> = A extends Readonly<Array<infer I>>
+  ? UnwrapReadonlyArrayType<I>
+  : A;
+type DT = InstanceType<typeof VDataTable>;
+type ReadonlyDataTableHeader = UnwrapReadonlyArrayType<DT['headers']>;
 
 interface Props {
   transactions: Transaction[];
@@ -34,6 +37,22 @@ interface Props {
 }
 
 const props = defineProps<Props>();
+
+const itemsPerPage = 50;
+
+const headers: ReadonlyDataTableHeader[] = [
+  {
+    title: 'Date/Time',
+    align: 'start',
+    key: 'dateAndTime',
+  },
+  { title: 'Description', align: 'start', key: 'description', sortable: false },
+  { title: 'Amount', align: 'start', key: 'amount' },
+];
+
+function getColor(value: string) {
+  return Number(value.substring(1)) > 0 ? 'green' : 'red';
+}
 
 function buildDescription(transaction: Transaction): string {
   const { quantity, symbol, price } = transaction;
@@ -53,19 +72,17 @@ function calculateAmount(transaction: Transaction): string {
   return transactionType == TransactionType.Buy ? `-${amount}` : amount;
 }
 
-function isBuy(transaction: Transaction): string {
-  return transaction.transactionType == TransactionType.Sell
-    ? '#228B22'
-    : '#FF0000';
-}
+type ProcessedTransactions = {
+  dateAndTime: string;
+  description: string;
+  amount: string;
+};
+
+const processedTransactions = props.transactions.map((t) => {
+  return {
+    dateAndTime: formatDate(t.dateAndTimeOfTransaction),
+    description: buildDescription(t),
+    amount: `${props.currency}${calculateAmount(t)}`,
+  } as ProcessedTransactions;
+});
 </script>
-
-<style scoped>
-#transactions-table {
-  margin-top: 25px;
-}
-
-#date-column {
-  width: 30%;
-}
-</style>
