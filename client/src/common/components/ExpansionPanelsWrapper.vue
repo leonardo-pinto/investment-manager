9878d6b080f6aa67713a4ab332dc990f47438a8a
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-10-27 12:55:46 -0400

    feat(common-components): add ExpansionPanelsWrapper

---
+++ client/src/common/components/ExpansionPanelsWrapper.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel>
+      <v-expansion-panel-title>
+        {{ props.title }}
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <slot></slot>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+<script setup lang="ts">
+interface Props {
+  title: string;
+}
+const props = defineProps<Props>();
+</script>
