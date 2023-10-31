94a4ff6334d99531b3a712625d38bd252294ee4d
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-10-31 17:14:48 -0400

    test(StockQuotesController): update unit tests with IStockQuoteService

--- server/tests/InvestmentManager.UnitTests/Controllers/StockQuotesControllerTest.cs
+++ server/tests/InvestmentManager.UnitTests/Controllers/StockQuotesControllerTest.cs
@@ -11,21 +11,19 @@ namespace InvestmentManager.UnitTests.Controllers
 {
     public class StockQuotesControllerTest
     {
-        private readonly Mock<IFinnhubService> _finnhubServiceMock;
-        private readonly Mock<IBrApiService> _brApiServiceMock;
+        private readonly Mock<IStockQuoteService> _stockQuoteServiceMock;
         private readonly StockQuotesController _sut;
 
         public StockQuotesControllerTest()
         {
-            _finnhubServiceMock = new Mock<IFinnhubService>(MockBehavior.Strict);
-            _brApiServiceMock = new Mock<IBrApiService>(MockBehavior.Strict);
-            _sut = new StockQuotesController(_finnhubServiceMock.Object, _brApiServiceMock.Object);
+            _stockQuoteServiceMock = new Mock<IStockQuoteService>(MockBehavior.Strict);
+            _sut = new StockQuotesController(_stockQuoteServiceMock.Object);
         }
 
         #region GetMultipleBrStockQuotes
 
         [Fact]
-        public async Task GetMultipleBrStockQuotes_ValidData_ToBeOk()
+        public async Task GetStockQuotes_ValidData_ToBeOk()
         {
             // Arrange
             string symbols = "PETR4,MGLU3,VALE3";
@@ -37,48 +35,17 @@ namespace InvestmentManager.UnitTests.Controllers
                 { new StockQuoteResult() { Symbol = "VALE3", Price = 80.20 } }
             };
 
-            _brApiServiceMock
-                .Setup(m => m.GetStocksPriceQuote(It.IsAny<string>()))
+            _stockQuoteServiceMock
+                .Setup(m => m.GetStockPriceQuote(It.IsAny<string>(), It.IsAny<string>()))
                 .ReturnsAsync(stockQuotesMock);
 
             // Act
-            var result = await _sut.GetBrStockQuotes(symbols);
+            var result = await _sut.GetStockQuotes(symbols, "BR");
 
             // Assert
             result.Should().BeOfType<ActionResult<StockQuotesResponse>>();
             var okObjectResult = result.Result as OkObjectResult;
             okObjectResult?.Value.Should().BeOfType<StockQuotesResponse>();
-        }
-        #endregion
-
-        #region GetUsStockQuotes
-
-        [Fact]
-        public async Task GetUsStockQuotes_ValidData_ToBeOk()
-        {
-            // Arrange
-            string symbols = "AMZN,VOO,VNQ";
-            var stockQuotesMock = new List<StockQuoteResult>()
-            {
-                { new StockQuoteResult(){ Symbol = "AMZN", Price = 100.50 } },
-                { new StockQuoteResult(){ Symbol = "VOO", Price = 372.99 } },
-                { new StockQuoteResult() { Symbol = "BK", Price = 40.20 } }
-            };
-
-            string[] expectedStockSymbols = { "AMZN", "VOO", "VNQ" };
-
-            _finnhubServiceMock
-                .Setup(m => m.GetMultipleStockPriceQuote(It.IsAny<string[]>()))
-                .ReturnsAsync(stockQuotesMock);
-
-            // Act
-            var result = await _sut.GetUsStockQuotes(symbols);
-
-            // Assert
-            result.Should().BeOfType<ActionResult<StockQuotesResponse>>();
-            var okObjectResult = result.Result as OkObjectResult;
-            okObjectResult?.Value.Should().BeOfType<StockQuotesResponse>();
-            _finnhubServiceMock.Verify(m => m.GetMultipleStockPriceQuote(expectedStockSymbols), Times.Once);
         }
         #endregion
     }
