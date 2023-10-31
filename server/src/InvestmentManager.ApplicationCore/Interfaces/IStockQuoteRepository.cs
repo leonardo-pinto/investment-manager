533f001529bce5de638886252bd8ddc20fe33684
leonardo-pinto <leonardoan.pinto@gmail.com>
2023-10-31 17:08:23 -0400

    feat(StockQuoteRepository): add IStockQuoteRepositoryinterface to centralize finnhub and brapi api calls

---
+++ server/src/InvestmentManager.ApplicationCore/Interfaces/IStockQuoteRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace InvestmentManager.ApplicationCore.Interfaces
+{
+    public interface IStockQuoteRepository
+    {
+        /// <summary>
+        /// Get a stock current price quote
+        /// </summary>
+        /// <param name="stockSymbol">Stock symbol to search</param>
+        /// <returns>Current price of the stock</returns>
+        Task<double> GetStockPriceQuote(string stockSymbol);
+    }
+}
